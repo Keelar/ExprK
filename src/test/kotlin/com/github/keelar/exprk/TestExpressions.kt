b4173d5dc206280fac9c00e0e3804c1e70525b3d
Wittawat Manha <36229606+Wireless4024@users.noreply.github.com>
2019-09-10 08:29:39 +0700

    make scanner scan scientific number correctly and add test

--- src/test/kotlin/com/github/keelar/exprk/TestExpressions.kt
+++ src/test/kotlin/com/github/keelar/exprk/TestExpressions.kt
@@ -13,4 +13,23 @@ class TestExpressions {
         expr.define("SCIVAL", scival)
         assertEquals(scival.toPlainString(), expr.eval("SCIVAL").toPlainString())
     }
+
+    @Test
+    fun `test Scanner will scan scientific form correctly`(){
+        val expr = Expressions()
+        assertEquals(BigDecimal("1e+7").toPlainString(), expr.eval("1E+7").toPlainString())
+        assertEquals(BigDecimal("1e-7").toPlainString(), expr.eval("1E-7").toPlainString())
+        assertEquals(BigDecimal(".101e+2").toPlainString(), expr.eval(".101e+2").toPlainString())
+        assertEquals(BigDecimal(".123e2").toPlainString(), expr.eval(".123e2").toPlainString())
+        assertEquals(BigDecimal("3212.123e-2").toPlainString(), expr.eval("3212.123e-2").toPlainString())
+    }
+
+    @Test
+    fun `test normal expression`() {
+        val expr = Expressions()
+        assertEquals(BigDecimal(".123e2").add(BigDecimal("3212.123e-2")).toPlainString(),
+                     expr.eval(".123e2+3212.123e-2").toPlainString())
+        assertEquals(BigDecimal("1e+7").minus(BigDecimal("52132e-2")).toPlainString(),
+                     expr.eval("1E+7-52132e-2").toPlainString())
+    }
 }
